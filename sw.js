fa11f47cd7a9731de157fb5187cc84d9a1a5f0d1
GorillaPodcast <ola2154314@maricopa.edu>
2026-02-16 02:10:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHE_NAME = ‘vibecheck-v1’;
+const CACHE_NAME = "vibecheck-v1";
 const urlsToCache = [
-‘./’,
-‘./index.html’,
-‘./manifest.json’
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-self.addEventListener(‘install’, event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
-self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener(‘activate’, event => {
-event.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener(‘fetch’, event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
